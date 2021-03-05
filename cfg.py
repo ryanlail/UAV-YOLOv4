a85ef53a2d1cae9659d88720e75c54652b895a53
Ryan Lail <me@ryanlail.com>
2021-03-05 19:06:16 +0000

    batch size

--- cfg.py
+++ cfg.py
@@ -22,7 +22,7 @@ Cfg.use_darknet_cfg = True
 Cfg.cfgfile = os.path.join(_BASE_DIR, 'cfg', 'yolov4-custom.cfg')
 
 Cfg.batch = 1
-Cfg.subdivisions = 16
+Cfg.subdivisions = 1
 Cfg.width = 608
 Cfg.height = 608
 Cfg.channels = 3
