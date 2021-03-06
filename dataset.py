7c46e678bd4c7b13ade9abb9bf0da3c60f62036c
Ryan Lail <me@ryanlail.com>
2021-03-06 19:37:40 +0000

    implement get image id function

--- dataset.py
+++ dataset.py
@@ -423,11 +423,11 @@ def get_image_id(filename:str) -> int:
     >>> no = f"{int(no):04d}"
     >>> return int(lv+no)
     """
-    raise NotImplementedError("Create your own 'get_image_id' function")
-    lv, no = os.path.splitext(os.path.basename(filename))[0].split("_")
-    lv = lv.replace("level", "")
-    no = f"{int(no):04d}"
-    return int(lv+no)
+
+    filename = filename.replace("image-rect-color-", "")
+    filename = filename.replace(".json", "")
+    filename = filename.replace(".", "")
+    return int(filename)
 
 
 if __name__ == "__main__":
